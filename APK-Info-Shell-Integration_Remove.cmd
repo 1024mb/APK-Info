a26a9e0f2f64cb3314b27d5fe9dc64294e77543a
Ángel <1024mb@users.noreply.github.com>
2023-12-22 01:31:48 -0500

    Update APK-Info-Shell-Integration_Remove.cmd
    delete whole branch

--- APK-Info-Shell-Integration_Remove.cmd
+++ APK-Info-Shell-Integration_Remove.cmd
@@ -7,12 +7,12 @@ SET "ERROR=0"
 REG DELETE "HKCU\SOFTWARE\Classes\.apk" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
 REG DELETE "HKCU\SOFTWARE\Classes\APK-Info.apk" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
 
-REG DELETE "HKCU\SOFTWARE\Classes\SystemFileAssociations\.apk\Shell\APK-Info\Command" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
+REG DELETE "HKCU\SOFTWARE\Classes\SystemFileAssociations\.apk" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
 
 REG DELETE "HKCU\SOFTWARE\Classes\.apks" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
 REG DELETE "HKCU\SOFTWARE\Classes\APK-Info.apks" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
 
-REG DELETE "HKCU\SOFTWARE\Classes\SystemFileAssociations\.apks\Shell\APK-Info\Command" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
+REG DELETE "HKCU\SOFTWARE\Classes\SystemFileAssociations\.apks" /f >nul 2>nul || SET "ERROR=1" >nul 2>nul
 
 IF %ERROR% EQU 0 (
 ECHO APK-Info shell integration removed!
