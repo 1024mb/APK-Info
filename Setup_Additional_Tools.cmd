17eead59b665180a58bd875969bd6f9aa76bf011
Ángel <1024mb@users.noreply.github.com>
2025-04-12 01:43:05 -0500

    refactor: use colon (:) instead of dot (.) for empty lines
    using a dot it's slower and in rare cases it can cause problems

--- Setup_Additional_Tools.cmd
+++ Setup_Additional_Tools.cmd
@@ -1,24 +1,24 @@
 @ECHO OFF
 SETLOCAL
 CLS
-ECHO.
+ECHO:
 ECHO Before continuing with the download you have to
 ECHO go to each of the following websites and accept
 ECHO their licenses.
-ECHO.
+ECHO:
 ECHO https://curl.se
 ECHO https://www.7-zip.org
 ECHO https://imagemagick.org
 ECHO https://developer.android.com/tools
-ECHO.
+ECHO:
 ECHO By continuing you agree with said licenses.
-ECHO.
+ECHO:
 CHOICE /C YN /M "Enter Y to confirm or N to exit."
 IF %ERRORLEVEL% NEQ 1 (
 	EXIT
 )
 CLS
-ECHO.
+ECHO:
 
 IF NOT EXIST .\tools MKDIR tools
 IF NOT EXIST .\tools\tmp MKDIR tools\tmp
@@ -33,9 +33,9 @@ IF DEFINED ProgramFiles(x86) (
 	SET "SEVENZIP_BIT= "
 )
 
-ECHO.
+ECHO:
 ECHO Getting latest 7-zip version
-ECHO.
+ECHO:
 powershell -Command "Invoke-WebRequest https://api.github.com/repos/ip7z/7zip/tags -OutFile .\tools\tmp\7zip-tags.json" 
 powershell -Command "$json=Get-Content -Raw -Path '.\tools\tmp\7zip-tags.json' | Out-String | ConvertFrom-Json; foreach ($line in $json) { $line.name > .\tools\tmp\7zip-latest.txt; return }"
 
@@ -51,33 +51,33 @@ FOR /F "delims=" %%G IN (.\tools\tmp\7zip-latest.txt) DO SET "SEVENZIP_VERSION_P
 DEL /F /Q .\tools\tmp\7zip-tags.json
 DEL /F /Q .\tools\tmp\7zip-latest.txt
 
-ECHO.
+ECHO:
 ECHO Downloading 7-zip
-ECHO.
+ECHO:
 powershell -Command "Invoke-WebRequest https://github.com/ip7z/7zip/releases/download/%SEVENZIP_VERSION%/7zr.exe -OutFile .\tools\tmp\7zr.exe"
 powershell -Command "Invoke-WebRequest https://github.com/ip7z/7zip/releases/download/%SEVENZIP_VERSION%/7z%SEVENZIP_VERSION_PLAIN%-extra.7z -OutFile .\tools\tmp\7z-extra.7z"
 
-ECHO.
+ECHO:
 ECHO Unpacking 7-zip...
-ECHO.
+ECHO:
 .\tools\tmp\7zr.exe e .\tools\tmp\7z-extra.7z -o.\tools %SEVENZIP_BIT%7za.exe -aoa -y
 REN .\tools\7za.exe 7z.exe
 DEL /F /Q .\tools\tmp\7zr.exe
 DEL /F /Q .\tools\tmp\7z-extra.7z
 
 ECHO Downloading curl...
-ECHO.
+ECHO:
 powershell -Command "Invoke-WebRequest https://curl.se/windows/latest.cgi?p=%CURL_BIT%-mingw.zip -OutFile .\tools\tmp\curl-%CURL_BIT%-mingw.zip"
 
-ECHO.
+ECHO:
 ECHO Extracting curl...
-ECHO.
+ECHO:
 .\tools\7z.exe e .\tools\tmp\curl-%CURL_BIT%-mingw.zip -o.\tools curl.exe curl-ca-bundle.crt -r -aoa -y
 DEL /F /Q .\tools\tmp\curl-%CURL_BIT%-mingw.zip
 
-ECHO.
+ECHO:
 ECHO Getting latest magick version
-ECHO.
+ECHO:
 powershell -Command "Invoke-WebRequest https://api.github.com/repos/ImageMagick/ImageMagick/tags -OutFile .\tools\tmp\magick-tags.json" 
 powershell -Command "$json=Get-Content -Raw -Path '.\tools\tmp\magick-tags.json' | Out-String | ConvertFrom-Json; foreach ($line in $json) { $line.name > .\tools\tmp\magick-latest.txt; return }"
 
@@ -87,27 +87,27 @@ FOR /F "delims=" %%G IN (.\tools\tmp\magick-latest.txt) DO SET "MAGICK_VERSION=%
 DEL /F /Q .\tools\tmp\magick-tags.json
 DEL /F /Q .\tools\tmp\magick-latest.txt
 
-ECHO.
+ECHO:
 ECHO Downloading magick
-ECHO.
+ECHO:
 .\tools\curl.exe -L -o .\tools\tmp\ImageMagick-%MAGICK_VERSION%-portable-Q16-%MAGICK_BIT%.zip https://imagemagick.org/archive/binaries/ImageMagick-%MAGICK_VERSION%-portable-Q16-%MAGICK_BIT%.zip
 
-ECHO.
+ECHO:
 ECHO Extracting magick...
-ECHO.
+ECHO:
 .\tools\7z.exe e .\tools\tmp\ImageMagick-%MAGICK_VERSION%-portable-Q16-%MAGICK_BIT%.zip -o.\tools magick.exe colors.xml -r -aoa -y
 DEL /F /Q .\tools\tmp\ImageMagick-%MAGICK_VERSION%-portable-Q16-%MAGICK_BIT%.zip
 
 
-ECHO.
+ECHO:
 ECHO Downloading Android platform-tools
-ECHO.
+ECHO:
 .\tools\curl.exe -o .\tools\tmp\platform-tools_r34.0.4-windows.zip https://dl.google.com/android/repository/platform-tools_r34.0.4-windows.zip
-ECHO.
+ECHO:
 ECHO Downloading Android build-tools
-ECHO.
+ECHO:
 .\tools\curl.exe -o .\tools\tmp\build-tools_r34-windows.zip https://dl.google.com/android/repository/build-tools_r34-windows.zip
-ECHO.
+ECHO:
 
 .\tools\7z.exe e .\tools\tmp\platform-tools_r34.0.4-windows.zip -o.\tools adb.exe AdbWinApi.dll AdbWinUsbApi.dll -r -aoa -y
 DEL /F /Q .\tools\tmp\platform-tools_r34.0.4-windows.zip
@@ -115,17 +115,17 @@ DEL /F /Q .\tools\tmp\platform-tools_r34.0.4-windows.zip
 .\tools\7z.exe e .\tools\tmp\build-tools_r34-windows.zip -o.\tools android-14\aapt.exe android-14\aapt2.exe android-14\lib\apksigner.jar android-14\libwinpthread-1.dll -aoa -y
 DEL /F /Q .\tools\tmp\build-tools_r34-windows.zip
 
-ECHO.
+ECHO:
 ECHO Downloading ADB compatible with WinXP
-ECHO.
+ECHO:
 IF NOT EXIST .\tools\xp MKDIR .\tools\xp
 .\tools\curl.exe -L -o .\tools\xp\adb.exe https://raw.githubusercontent.com/1024mb/adb-win/master/SDK_Platform-Tools_for_Windows/platform-tools_r23.1.0-windows/adb.exe
 .\tools\curl.exe -L -o .\tools\xp\AdbWinApi.dll https://raw.githubusercontent.com/1024mb/adb-win/master/SDK_Platform-Tools_for_Windows/platform-tools_r23.1.0-windows/AdbWinApi.dll
 .\tools\curl.exe -L -o .\tools\xp\AdbWinUsbApi.dll https://raw.githubusercontent.com/1024mb/adb-win/master/SDK_Platform-Tools_for_Windows/platform-tools_r23.1.0-windows/AdbWinUsbApi.dll
 
 RMDIR /S /Q .\tools\tmp
-ECHO.
-ECHO.
+ECHO:
+ECHO:
 ECHO All tools downloaded.
 ECHO Press any button to close this window.
 PAUSE > NUL 2>&1
